c2d8d6056997ed19a68b604bda4ecaad828b46d7
rafal <redaf@users.noreply.github.com>
2024-12-04 01:26:29 +0100

    test pass

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -2,8 +2,27 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn part_1_sum_of_uncorrupted_muls(memory: &str) -> u32 {
-    todo!()
+fn part_1_sum_of_uncorrupted_muls(memory: &str) -> i32 {
+    memory
+        .split("mul(")
+        .filter(|ins| ins.contains(",") && ins.contains(")"))
+        .filter_map(|ins| match ins.find(')') {
+            Some(i) => Some(ins.split_at(i).0),
+            None => None,
+        })
+        .filter_map(|args| {
+            let mut args = args.split(",");
+            match (args.next(), args.next()) {
+                (Some(a), Some(b)) => Some((a, b)),
+                _ => None,
+            }
+        })
+        .filter_map(|(a, b)| match (a.parse::<i32>(), b.parse::<i32>()) {
+            (Ok(a), Ok(b)) => Some((a, b)),
+            _ => None,
+        })
+        .map(|(a, b)| a * b)
+        .sum()
 }
 
 #[cfg(test)]
